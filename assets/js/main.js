7900931bbf7f51765e65696f61e25db2def67f79
Murilo <dev.murilosopi@gmail.com>
2022-07-08 19:05:43 -0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function goAllTasks() {
+  location.href = "all_tasks.html";
+}
+
+function goNewTask() {
+  location.href = "new_task.html";
+}
+
+function goHome() {
+  location.href = "home.html";
+}
