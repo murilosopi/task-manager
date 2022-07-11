cb3b8be5cb7ee9096c32e38a04e66cdb7b155905
Murilo <dev.murilosopi@gmail.com>
2022-07-11 17:46:55 -0300

    Create handler for nav toggler

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,18 @@ function goCreateAccount() {
 function goLogin() {
   location.href = "index.php";
 }
+
+(function addEventListeners() {
+  const navToggler = document.querySelector("nav .nav-toggler");
+  navToggler.addEventListener('click', navToggleHandler);
+})()
+
+function navToggleHandler() {
+  const nav = this.parentNode;
+  const navMenu = nav.querySelector(".menu");
+  if(navMenu.classList.contains("is-hidden")) {
+    navMenu.classList.remove("is-hidden");
+  } else {
+    navMenu.classList.add("is-hidden");
+  }
+}
